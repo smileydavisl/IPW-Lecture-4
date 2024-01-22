14c434b4ceb764cd35f902cd9004520705da3b57
smileydavisl <129325547+smileydavisl@users.noreply.github.com>
2024-01-22 16:46:15 -0700

    added .h file

---
+++ functions_to_implement.h
@@ -0,0 +1,8 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string Join(const vector<string> pieces, const string glue);
+int Sign(int num);
+double Sum(const vector<double>nums);
